a9f1946134aeb6f91071ab177cb0e488bf745b4b
Emmanuel ELIASON-ARMSTRONG <geteliarms@gmail.com>
2020-11-15 15:06:36 +0700

    Dockerizing a Rail Application

---
+++ rails/config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Hello
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Log to STDOUT because Docker expects all processes to log here. You could
+    # then redirect logs to a third party service on your own such as systemd,
+    # or a third party host such as Loggly, etc..
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+end
